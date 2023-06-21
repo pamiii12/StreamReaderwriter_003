22434b69af64008cee1f50b3cd197d5558c45e32
pamiii12 <fahmiadityaa03@gmail.com>
2023-06-21 15:21:39 +0700

    library

---
+++ ioFileWithPuth/ioFileWithPuth.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukkan Nama File : ";
+	cin >> NamaFile;
+
+
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjukkan ke sebuah nama file
+	outfile.open(NamaFile +".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	// Unlimited loop untuk menulis.
+	while (true) {
+		cout << "_ ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		// Menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	// Selesai dalam menulis sekarang tutup file nya
+	outfile.close();
+
+	// Membuka file dalam mode membaca
+	ifstream infile;
+	// Menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	// Jika file ada maka
+	if (infile.is_open())
+	{
+		// Melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tambilkan disini
+			cout << baris << '\n';
+		}
+		// Tutup file tersebut setelah selesai
+		infile.close();
+	}
+	// Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+
+
+}
